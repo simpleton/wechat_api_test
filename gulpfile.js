dab255731bfea1466cdf965638f4143d7ea9c730
simsun <sunsj1231@gmail.com>
2014-11-07 20:30:06 +0800

    commit gulp config files

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,79 @@
 var gulp = require('gulp')
 
-var coffee = require('gulp-coffee');
-var gutil = require('gulp-util');
-var runSequence = require('run-sequence');
+var plugins = require("gulp-load-plugins")({lazy: false, camelize: true});
+var del = require('del')
 
 gulp.task('coffee', function() {
   gulp.src('./src/*.coffee')
-    .pipe(coffee({bare: true}).on('error', gutil.log))
-    .pipe(gulp.dest('./public/'))
+    .pipe(plugins.coffee({bare: true}).on('error', plugins.util.log))
+    .pipe(gulp.dest('./dist/'))
 });
 
 // Jade to HTML
 gulp.task('jade', function() {
   return gulp.src("./src/jade/*.jade")
-            .pipe(plugins.jade({
+sudo             .pipe(plugins.jade({
               'pretty': true,
               'locals': yaml.safeLoad(fs.readFileSync('./src/db/data.yml', 'utf8'))
             }))
             .pipe(gulp.dest('./dist'))
 });
 
-gulp.task('default', ['coffee'])
+// Compile SCSS as compressed CSS
+gulp.task('sass', function() {
+  return gulp.src(['./src/scss/app.scss',
+                  './src/scss/**/*.sass'
+                  ])
+            .pipe(plugins.rubySass({
+              loadPath: [loadPath[0],loadPath[1]]
+            }))
+            .on('error', function(err) {console.log(err.message);})
+            .pipe(plugins.concat('app.css'))
+            // .pipe(plugins.minifyCss({keepBreaks: true}))
+            .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('connect', plugins.connect.server({
+    root: ['dist'],
+    port: 9000,
+    livereload: true
+}));
+
+gulp.task('watch',function(){
+    gulp.watch([
+        'dist/**/*.html',
+        'dist/**/*.js',
+        'dist/**/*.css',
+        '!dist/bower_components/**/*.js',
+        '!dist/bower_components/**/*.html',
+    ], function(event) {
+        return gulp.src(event.path)
+            .pipe(plugins.connect.reload());
+    });
+    gulp.watch(['!./src/**/*_test.js','./src/js/**/*.js', '!./src/bower_components/**/*.js'],['scripts']);
+    gulp.watch(['./src/jade/**/*.jade'],['jade']);
+    gulp.watch(['./src/*.coffee'],['coffee']);
+    gulp.watch(['./src/scss/app.scss','./src/scss/**/*.sass'],['sass']);
+
+});
+
+gulp.task('lint', function () {
+  gulp.src('./**/*.js')
+    .pipe(plugins.jshint())
+})
+
+gulp.task('develop', function () {
+  plugins.nodemon({ script: 'dist/app.js', ext: 'html js', ignore: ['ignored.js'] })
+    .on('change', ['lint'])
+    .on('restart', function () {
+      console.log('restarted!')
+    })
+})
+
+// Delete the dist directory
+gulp.task('clean', function(cb) {
+  del(['./dist'], cb);
+});
+
+gulp.task('default', ['develop', 'coffee' ,'watch'])
+gulp.task('build', ['clean', 'coffee'])
--- src/app.coffee
+++ src/app.coffee
@@ -1,7 +1,7 @@
 http = require('http')
 
-server = http.createServer(req, res) ->
-  res.writeHEAD 200
-  res.end 'Hello World'
+server = http.createServer (req, res) ->
+  res.writeHead 200
+  res.end 'Hello World!!'
 
 server.listen 8080
